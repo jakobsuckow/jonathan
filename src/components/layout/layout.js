ddc41c18f45a0189db384bfc23f3080d31e29d62
Jakob Suckow <jakob@martian.agency>
2020-01-11 17:50:50 +0100

    now with menu animations

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,35 +1,58 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 import "./module.scss"
+import { Link } from "gatsby"
 
 const Layout = ({ children }) => {
   const [isOpenSocial, setIsOpenSocial] = useState(false)
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
 
   const socialTrigger = () => {
     setIsOpenSocial(!isOpenSocial)
   }
+  const menuTrigger = () => {
+    setIsOpenMenu(!isOpenMenu)
+  }
+
+  const closeMenu = () => {
+    setIsOpenMenu(false)
+  }
+
   return (
     <>
-    <div class={`social ${isOpenSocial ? 'open': ''}`}>
-     <ul>
-       <li>Social 1</li>
-       <li>Social 2</li>
-       <li>Social 3</li>
-       <li>Social 4</li>
-     </ul>
-    </div>
-    <div className={`global ${isOpenSocial ? 'open': ''}`}>
-      <div className="menu">
-        <span className="burgerButton">button 1</span>
-        <span className="socialButton" onClick={socialTrigger}>button 2</span>
+      <div class={`social ${isOpenSocial ? "open" : ""}`}>
+        <ul>
+          <li>Social 1</li>
+          <li>Social 2</li>
+          <li>Social 3</li>
+          <li>Social 4</li>
+        </ul>
       </div>
-      <div>
-        <div className="body">{children}</div>
-        <footer>
-          © {new Date().getFullYear()}, <p>Jonathan Moeller</p>
-        </footer>
+      <div className={`menu ${isOpenMenu ? "open" : ""}`}>
+        <div class="menu-items">
+          <span onClick={closeMenu}>Close Menu</span>
+          <Link to="/">Home</Link>
+          <Link to="/">Experience</Link>
+          <Link to="/">Skils</Link>
+          <Link to="/">About Me</Link>
+        </div>
       </div>
-    </div>
+      <div className={`global ${isOpenSocial ? "open" : ""}`}>
+        <div className="header">
+          <span className="burgerButton" onClick={menuTrigger}>
+            button 1
+          </span>
+          <span className="socialButton" onClick={socialTrigger}>
+            button 2
+          </span>
+        </div>
+        <div>
+          <div className="body">{children}</div>
+          <footer>
+            © {new Date().getFullYear()}, <p>Jonathan Moeller</p>
+          </footer>
+        </div>
+      </div>
     </>
   )
 }
